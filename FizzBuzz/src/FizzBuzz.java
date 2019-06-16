7e98ce79b8b3ec1677347eb918ae952770b9bb27
Ralph Boilard <fulmen1298@gmail.com>
2019-06-16 14:40:23 -0500

    First Push All Files

---
+++ FizzBuzz/src/FizzBuzz.java
@@ -0,0 +1,21 @@
+
+public class FizzBuzz {
+	public String fizzBuzz(int number) {
+		String result;
+		if(number % 3 == 0 && number % 5 == 0) {
+			result = "FizzBuzz";
+			
+		}
+		else if(number % 3 == 0) {
+			result = "Fizz";
+		}
+		else if(number % 5 == 0) {
+			result = "Buzz";
+		}
+		else {
+			result = String.format("%d", number);
+		}
+		return result;
+	}
+
+}
---
+++ FizzBuzz/src/FizzBuzzTest.java
@@ -0,0 +1,9 @@
+
+public class FizzBuzzTest {
+	public static void main(String[] args) {
+		FizzBuzz fB = new FizzBuzz();
+		String output = fB.fizzBuzz(19);
+		System.out.println(output);
+	}
+
+}
